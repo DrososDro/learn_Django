7667547f017a344f851998387c6e17e8d0296957
DrososDro <Drosinakis.drosos1@gmail.com>
2022-07-06 23:38:24 +0300

    i set up my first uls

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def projects(request):
+    return HttpResponse("This is my page")
+
+def project(request, pk):
+    return HttpResponse(f"This is the {pk} product")
