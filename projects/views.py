a6fb7d4b5d0e4dfa3d581bb93476f13ad88ae809
DrososDro <Drosinakis.drosos1@gmail.com>
2022-07-07 21:05:06 +0300

    add temp and play with them

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
+
 def projects(request):
-    return HttpResponse("This is my page")
+    # return HttpResponse("This is my page")
+    return render(request, "projects/projects.html")
+
 
 def project(request, pk):
-    return HttpResponse(f"This is the {pk} product")
+    # return HttpResponse(f"This is the {pk} product")
+    return render(request, "projects/single-project.html")
